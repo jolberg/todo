477695a06e85709d9a58c4331fea7a4b738b24d3
jolberg <olli_ti@hotmail.com>
2020-12-07 21:04:53 +0200

    palautettava versio

---
+++ src/Todo.js
@@ -0,0 +1,12 @@
+
+
+export class Todo {
+    title = '';
+    description = ''
+
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+    }
+}
+export default Todo;
